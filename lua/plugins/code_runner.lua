9d3ce4409a5eedd5b41fe4f7659e467cac62de8b
Newton dos Santos <neewrobert@gmail.com>
2024-09-18 17:38:12 +0200

    improving the folder organization when using code runner to compile and run a java code

--- lua/plugins/code_runner.lua
+++ lua/plugins/code_runner.lua
@@ -6,12 +6,51 @@ return {
   config = function()
     require('code_runner').setup({
       filetype = {
-        java = {
-          "cd $dir &&",
-          "mkdir -p ../out/src &&",  -- Create the out directory if it doesn't exist
-          "javac -d ../out/src $fileName &&",  -- Compile the Java file and put the .class files in the out directory
-          "java -cp ../out/src $fileNameWithoutExt"  -- Run the compiled class from the out directory
-        },
+        java = function()
+          -- Get the full path of the current file
+          local filePath = vim.fn.expand('%:p')
+          local fileName = vim.fn.expand('%:t')
+          local fileNameWithoutExt = vim.fn.expand('%:t:r')
+
+          -- Find the project root (assumed to be the directory containing 'src')
+          local projectRoot = vim.fn.getcwd()
+          while projectRoot ~= '/' and not vim.loop.fs_stat(projectRoot .. '/src') do
+            projectRoot = vim.fn.fnamemodify(projectRoot, ':h')
+          end
+          if projectRoot == '/' then
+            projectRoot = vim.fn.getcwd()
+          end
+
+          -- Define source and output directories
+          local srcDir = projectRoot .. '/src'
+          local outDir = projectRoot .. '/out'
+
+          -- Read the package name from the Java file
+          local packageName = ''
+          for line in io.lines(filePath) do
+            local match = string.match(line, '^%s*package%s+([%w%.]+)%s*;')
+            if match then
+              packageName = match
+              break
+            end
+          end
+
+          -- Build the fully qualified class name
+          local fullyQualifiedClassName = packageName ~= '' and (packageName .. '.' .. fileNameWithoutExt) or fileNameWithoutExt
+
+          -- Build the commands
+          local cmds = {
+            'cd ' .. projectRoot,
+            'mkdir -p out',
+            'find ' .. srcDir .. ' -name "*.java" > sources.txt',
+            'javac -d out @sources.txt',
+            'rm sources.txt',
+            'java -cp out ' .. fullyQualifiedClassName
+          }
+
+          -- Return the commands as a single string
+          return table.concat(cmds, ' && ')
+        end,
         python = "python3 -u",
         typescript = "deno run",
         rust = {
@@ -38,4 +77,3 @@ return {
     })
   end,
 }
-
